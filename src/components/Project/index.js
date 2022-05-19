5533ba2c590835543b41215c38586769a8e681fe
alen <alen.dunn1@gmail.com>
2022-05-18 21:26:51 -0400

    resume section and project pictures

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { useEffect } from "react";
 
 function Project() {
@@ -11,18 +12,21 @@ function Project() {
       desc: "Search for an artist to find upcoming concerts and tickets.",
       link: "https://adunny.github.io/concert-vagabond/",
       repo: "https://github.com/adunny/concert-vagabond",
+      img: 'https://i.imgur.com/KCTGBIy.png'
     },
     {
       name: "PodFreaks",
       desc: "Podcast forum to leave reviews for podcasts.",
       link: "https://evening-tor-85614.herokuapp.com/",
       repo: "https://github.com/NicoleBarranca/PodFreaks",
+      img: 'https://i.imgur.com/ru3cuvx.png'
     },
     {
       name: "Placeholder Project",
       desc: "Nulla culpa reprehenderit laboris laborum nulla pariatur tempor anim aliquip labore cupidatat deserunt eu aute.",
       link: "",
       repo: "",
+      img: 'https://placeholder.pics/svg/500x200'
     },
   ];
 
@@ -30,7 +34,7 @@ function Project() {
     <div className="container">
       <div className="row mt-3">
         <div className="col-12">
-          <p>Check out my projects</p>
+          
         </div>
       </div>
       {projects.map((project) => (
@@ -38,17 +42,21 @@ function Project() {
           <div className="row g-0">
             <div className="col-md-4">
               <img
-                src="https://placeholder.pics/svg/500x200"
+                src={project.img}
                 className="img-fluid rounded-start"
                 alt="..."
               />
             </div>
             <div className="col-md-8">
               <div className="card-body">
-                <h5 className="card-title">{project.name}</h5>
+                <h4 className="card-title">{project.name}</h4>
                 <p className="card-text">{project.desc}</p>
                 <p className="card-text">
-                  <small className="text-muted">(github icon here?)</small>
+                  <small className="text-muted">
+                    <a href={project.link} className="btn bg-secondary text-light">Website</a>
+                    <span>     </span>
+                    <a href={project.repo} className="btn bg-secondary text-light">Github</a>
+                  </small>
                 </p>
               </div>
             </div>
