fe416cb11b9fc5293b5caf6a0af6d49f397ac297
alen <alen.dunn1@gmail.com>
2022-06-13 15:59:09 -0400

    remove proxy

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../build')));
-    app.get((req, res) => {
-      res.sendFile(path.join(__dirname, '../../build/index.html'));
-    });
 };
+app.get((req, res) => {
+  res.sendFile(path.join(__dirname, '../../build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
