6a66b0dfa52897fb3ede56fd0b4650214c9b6297
alen <alen.dunn1@gmail.com>
2022-07-22 22:52:20 -0400

    remove resume page, link to resume instead

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
 // import { useState } from 'react';
-import './App.css';
-import About from './components/About';
-import ContactForm from './components/Contact/ContactForm';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Project from './components/Project';
-import Resume from './components/Resume';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import About from "./components/About";
+import ContactForm from "./components/Contact/ContactForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  // const [projects, projectsSelected] = useState(false);
-  // const [contact, contactSelected] = useState(false);
-  // const [resume, resumeSelected] = useState(false);
-  
   return (
     <Router>
-      <Header/>
+      <Header />
       <main>
         <Routes>
-          <Route 
-          path='/about'
-          element={<About/>}
-          />
-          <Route 
-          path='/projects'
-          element={<Project/>}
-          />
-          <Route 
-          path='/contact'
-          element={<ContactForm/>}
-          />
-          <Route
-          path='/resume'
-          element={<Resume/>}
-          />
-          <Route
-          path='*'
-          element={<About/>}
-          />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="*" element={<About />} />
         </Routes>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,43 +2,36 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
-function Header(props) {
-  // const { projectsSelected, contactSelected, resumeSelected } = props;
-
+function Header() {
   return (
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand as={Link} to='/'>Alen Dunn</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            Alen Dunn
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link
-                as={NavLink}
-                to="/about"
-              >
+              <Nav.Link as={NavLink} to="/about">
                 About
               </Nav.Link>
-              <Nav.Link
-               as={NavLink}
-               to='/projects'
-              >
+              <Nav.Link as={NavLink} to="/projects">
                 Projects
               </Nav.Link>
-              <Nav.Link
-                as={NavLink}
-                to='/contact'
-              >
+              <Nav.Link as={NavLink} to="/contact">
                 Contact
               </Nav.Link>
-              <Nav.Link
-                as={NavLink}
-                to='/resume'
+              <a
+                className="nav-link"
+                href="https://drive.google.com/file/d/1ehy3wNocEVk30gcMbPLM4JM_k21woF4M/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
               >
                 Resume
-              </Nav.Link>
+              </a>
             </Nav>
           </Navbar.Collapse>
         </Container>
